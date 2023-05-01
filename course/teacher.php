c5f294356d57489888b867b3e3dfab8106bd2b2d
lavisar <sarlavi6969@gmail.com>
2023-05-01 15:50:35 +0700

    login work correctly 1/5 - 15h50

---
+++ course/teacher.php
@@ -0,0 +1,3 @@
+<?php 
+    echo "teacher page";
+?>
--- login.php
+++ login.php
@@ -3,49 +3,115 @@
 
 <head>
     <title>Đăng nhập</title>
+    <style>
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 16px;
+        }
+
+        .container {
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            margin: 0 auto;
+            max-width: 400px;
+            padding: 20px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        input[type=text],
+        input[type=password] {
+            border: none;
+            border-radius: 3px;
+            box-shadow: 0 0 2px rgba(0, 0, 0, 0.3);
+            padding: 10px;
+            width: 100%;
+        }
+
+        input[type=submit] {
+            background-color: #4CAF50;
+            border: none;
+            border-radius: 3px;
+            color: #fff;
+            cursor: pointer;
+            margin-top: 10px;
+            padding: 10px;
+            width: 100%;
+        }
+
+        input[type=submit]:hover {
+            background-color: #3e8e41;
+        }
+
+        label {
+            display: block;
+            font-weight: bold;
+            margin-top: 10px;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Đăng nhập</h1>
-    <?php
-    if (isset($_POST['submit'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+    <div class="container">
+        <h1>Đăng nhập</h1>
+        <?php
+        if (isset($_POST['submit'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
 
-        // Kết nối đến cơ sở dữ liệu
-        $conn = mysqli_connect('localhost', '', '', '');
+            // Kết nối đến cơ sở dữ liệu
+            $conn = mysqli_connect('localhost', '', '', 'elearndb');
 
-        // Kiểm tra kết nối
-        if (!$conn) {
-            die("Kết nối cơ sở dữ liệu thất bại: " . mysqli_connect_error());
+            // Kiểm tra kết nối
+            if (!$conn) {
+                die("Kết nối cơ sở dữ liệu thất bại: " . mysqli_connect_error());
+            }
+
+            // Truy vấn để kiểm tra thông tin đăng nhập
+            $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+            $result = mysqli_query($conn, $sql);
+
+            // Kiểm tra kết quả truy vấn
+            if (mysqli_num_rows($result) > 0) {
+                // Lấy thông tin người dùng từ kết quả truy vấn
+                $row = mysqli_fetch_assoc($result);
+                $role = $row['role'];
+
+                // Kiểm tra giá trị của trường role
+                if ($role == 1) {
+                    // Nếu role = 1 thì chuyển hướng đến trang teacher.php
+                    session_start();
+                    $_SESSION['username'] = $username;
+                    header('Location: ./course/teacher.php');
+                } else {
+                    // Nếu role = 0 thì chuyển hướng đến trang student.php
+                    session_start();
+                    $_SESSION['username'] = $username;
+                    header('Location: ./course/student.php');
+                }
+            } else {
+                // Đăng nhập thất bại
+                echo "<p>Sai tên đăng nhập hoặc mật khẩu. Vui lòng thử lại.</p>";
+            }
+
+            // Đóng kết nối đến cơ sở dữ liệu
+            mysqli_close($conn);
         }
+        ?>
+        <form method="post">
+            <label>Tên đăng nhập:</label>
+            <input type="text" name="username" required>
 
-        // Truy vấn để kiểm tra thông tin đăng nhập
-        $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-        $result = mysqli_query($conn, $sql);
+            <label>Mật khẩu:</label>
+            <input type="password" name="password" required>
 
-        // Kiểm tra kết quả truy vấn
-        if (mysqli_num_rows($result) > 0) {
-            // Đăng nhập thành công
-            session_start();
-            $_SESSION['username'] = $username;
-            header('Location: dashboard.php');
-        } else {
-            // Đăng nhập thất bại
-            echo "<p>Sai tên đăng nhập hoặc mật khẩu. Vui lòng thử lại.</p>";
-        }
-
-        // Đóng kết nối đến cơ sở dữ liệu
-        mysqli_close($conn);
-    }
-    ?>
-    <form method="post">
-        <label>Tên đăng nhập:</label><br>
-        <input type="text" name="username"><br>
-        <label>Mật khẩu:</label><br>
-        <input type="password" name="password"><br><br>
-        <input type="submit" name="submit" value="Đăng nhập">
-    </form>
+            <input type="submit" name="submit" value="Đăng nhập">
+        </form>
+    </div>
 </body>
 
 </html>
