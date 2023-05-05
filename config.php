53f0a38400a288b69e5c6582797123256b282a9a
lavisar <sarlavi6969@gmail.com>
2023-05-05 23:21:02 +0700

    added config.php

--- config.php
+++ config.php
@@ -1,2 +1,21 @@
 <?php
-$domain = 'http://localhost/Elearn_web/courses/contactList.php';
+$domain_contactList = 'http://localhost/Elearn_web/courses/contactList.php';
+$domain_profileUpdate = 'http://localhost/Elearn_web/courses/updateProfile.php';
+$domain_actionPHP_Logout = 'http://localhost/Elearn_web/courses/action_php/logout.php';
+
+// domain for courses 
+// --------------------------------
+// Student
+$domain_courses_DataStructure = 'http://localhost/Elearn_web/courses/API_Youtube/student_courses/DataStructures.php';
+$domain_courses_Java = 'http://localhost/Elearn_web/courses/API_Youtube/student_courses/Java.php';
+$domain_courses_PHP = 'http://localhost/Elearn_web/courses/API_Youtube/student_courses/PHP.php';
+$domain_courses_Python = 'http://localhost/Elearn_web/courses/API_Youtube/student_courses/Python.php';
+$domain_courses_Web = 'http://localhost/Elearn_web/courses/API_Youtube/student_courses/Web.php';
+$domain_courses_Git = 'http://localhost/Elearn_web/courses/API_Youtube/student_courses/SourceControl.php';
+$domain_courses_MySQL = 'http://localhost/Elearn_web/courses/API_Youtube/student_courses/MySQL.php';
+// --------------------------------
+// Teacher
+$domain_courses_PedagogicalBehaviorSkills = 'http://localhost/Elearn_web/courses/API_Youtube/teacher_courses/PedagogicalBehaviorSkills.php';
+$domain_courses_Pedagogical = 'http://localhost/Elearn_web/courses/API_Youtube/teacher_courses/NghiepVuSP.php';
+$domain_courses_Presentation = 'http://localhost/Elearn_web/courses/API_Youtube/teacher_courses/PresentationSkill.php';
+$domain_courses_TeachingSkill = 'http://localhost/Elearn_web/courses/API_Youtube/teacher_courses/TeachingSkill.php';
