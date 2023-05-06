1ac0f65c3d2177ca7ae4bad1a4359ff47218da91
lavisar <sarlavi6969@gmail.com>
2023-05-06 12:12:45 +0700

    fix relative bug

--- courses/teacher.php
+++ courses/teacher.php
@@ -7,12 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Elearn | Teacher</title>
 </head>
+<?php include 'config.php' ?>
 
 <body>
     <!-- PHP kiểm tra đăng nhập và phân quyền -->
     <?php
     session_start();
-    require 'config.php';
     if ($_SESSION['role'] == 0) {
         // Nếu chưa sai quyền, chuyển hướng đến trang đăng nhập        
         header('Location: ../login.php');
